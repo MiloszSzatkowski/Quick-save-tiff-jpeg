9d9ede92b8d3b091e35274747ccbe6cf31ede789
MiloszSzatkowski <milosz.dawid.szatkowski@gmail.com>
2018-05-25 19:42:31 +0200

    Ready

---
+++ Quick_JPEG.jsx
@@ -0,0 +1,49 @@
+preferences.rulerUnits = Units.CM;
+
+saveState();
+
+app.activeDocument.flatten();
+
+//to RGB
+var id11 = charIDToTypeID( "CnvM" );
+    var desc4 = new ActionDescriptor();
+    var id12 = charIDToTypeID( "T   " );
+    var id13 = charIDToTypeID( "RGBM" );
+    desc4.putClass( id12, id13 );
+    var id14 = charIDToTypeID( "Fltt" );
+    desc4.putBoolean( id14, false );
+executeAction( id11, desc4, DialogModes.NO );
+
+app.activeDocument.resizeImage(
+  35,
+  null,
+  72,
+  ResampleMethod.BICUBIC
+);
+
+var Path = app.activeDocument.path;
+var Name = app.activeDocument.name.replace(/\.[^\.]+$/, '');
+var Suffix = "prev";
+var saveFile = File(Path + "/" + "_" + Name + "_" + Suffix + ".jpg");
+
+SaveJPEG(saveFile, 6);
+
+undo(historyStatus);
+
+
+function SaveJPEG(saveFile, jpegQuality){
+jpgSaveOptions = new JPEGSaveOptions();
+jpgSaveOptions.embedColorProfile = true;
+jpgSaveOptions.formatOptions = FormatOptions.STANDARDBASELINE;
+jpgSaveOptions.matte = MatteType.NONE;
+jpgSaveOptions.quality = jpegQuality;
+activeDocument.saveAs(saveFile, jpgSaveOptions, true, Extension.LOWERCASE);
+}
+
+function undo (state) {
+ app.activeDocument.activeHistoryState = state;
+}
+
+function saveState () {
+  historyStatus = app.activeDocument.activeHistoryState ;
+}
---
+++ Quick_TIFF.jsx
@@ -0,0 +1,40 @@
+preferences.rulerUnits = Units.CM;
+
+saveState();
+
+app.activeDocument.flatten();
+
+//to cmyk
+var idCnvM = charIDToTypeID( "CnvM" );
+    var desc54 = new ActionDescriptor();
+    var idT = charIDToTypeID( "T   " );
+    var idCMYM = charIDToTypeID( "CMYM" );
+    desc54.putClass( idT, idCMYM );
+executeAction( idCnvM, desc54, DialogModes.NO );
+
+var Path = app.activeDocument.path;
+var Name = app.activeDocument.name.replace(/\.[^\.]+$/, '');
+var Suffix = "print";
+var saveFile = File(Path + "/" + Name + "_" + Suffix + '.tif');
+
+SaveTIFF(saveFile);
+
+undo(historyStatus);
+
+
+function SaveTIFF(saveFile){
+tiffSaveOptions = new TiffSaveOptions();
+tiffSaveOptions.embedColorProfile = true;
+tiffSaveOptions.alphaChannels = true;
+tiffSaveOptions.layers = true;
+tiffSaveOptions.imageCompression = TIFFEncoding.TIFFLZW;
+activeDocument.saveAs(saveFile, tiffSaveOptions, true, Extension.LOWERCASE);
+}
+
+function undo (state) {
+ app.activeDocument.activeHistoryState = state;
+}
+
+function saveState () {
+  historyStatus = app.activeDocument.activeHistoryState ;
+}
