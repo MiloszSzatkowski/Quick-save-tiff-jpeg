f8ddeb274c358cf3900d7fd36b85ce3f6137993e
MiloszSzatkowski <milosz.dawid.szatkowski@gmail.com>
2018-05-26 09:39:09 +0200

    Added compatibility with multi page pdf's and it's working with several opened pdf's

--- Quick_TIFF.jsx
+++ Quick_TIFF.jsx
@@ -12,12 +12,42 @@ var idCnvM = charIDToTypeID( "CnvM" );
     desc54.putClass( idT, idCMYM );
 executeAction( idCnvM, desc54, DialogModes.NO );
 
-var Path,
-  temp;
+var Path, NameTemp, Name, temp;
 try {
   Path = app.activeDocument.path;
 } catch (e) {
-  Path = app.recentFiles[0].toString();
+
+    Path  = 0;
+
+    //decode and clean input
+    //Then delete numerical ending of multi page pdfs
+    NameTemp = decodeURI(app.activeDocument.name.toString().
+    replace(/((-\d\d)\.pdf)|((-\d)\.pdf)/g), '');
+
+    //if it was a single page pdf
+    NameTemp = NameTemp.replace('.pdf', '');
+
+    //if undefined was appended (yes, it may happen with regex)
+    NameTemp = NameTemp.replace('undefined', '')
+
+    Name = NameTemp;
+
+    for( var i =0; i < app.recentFiles.length; i++){
+
+       temp = decodeURI(app.recentFiles[i].toString()).split('/');
+       temp = temp[temp.length-1].replace('.pdf', '');
+
+       if ( temp == Name){
+          Path = decodeURI(app.recentFiles[i].toString());
+              break;
+       }
+    }
+}
+
+if(!Path){
+       alert ("Script couldn't find the path of opened file");
+} else {
+       // alert (Path);
 }
 
 var Name = app.activeDocument.name.replace(/\.[^\.]+$/, '');
